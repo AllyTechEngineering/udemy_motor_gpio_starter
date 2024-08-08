5188026f78f4c72ffafbee94f829c2f953d31f80
Bob Taylor <btaylor@allytechllc.com>
2024-08-08 15:25:52 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:udemy_motor_gpio/motor_screen/motor_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'UdeMy Motor GPIO',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MotorScreen(),
+    );
+  }
+}
---
+++ lib/motor_screen/motor_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MotorScreen extends StatefulWidget {
+  const MotorScreen({super.key});
+
+  @override
+  State<MotorScreen> createState() => _MotorScreenState();
+}
+
+class _MotorScreenState extends State<MotorScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+//TODO: Import the motor_service.dart file
+//TODO: Create an instance of the MotorService class
+//TODO: Call the initGpio16Output method in the initState method
+//TODO: Call the disposeGpio method in the dispose method
+//TODO: Create the getElevatedButtonMotorOn method
+//TODO: Create the getElevatedButtonMotorOff method
+//TODO: Create a Scaffold widget
+//TODO: Add an AppBar widget
+//TODO: Add a Center widget
+//TODO: Add a Column widget
+//TODO: Add the getElevatedButtonMotorOn method
+//TODO: Add the getElevatedButtonMotorOff method
